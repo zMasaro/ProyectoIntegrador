eb99c4919f25904f3109ed874a3349bb015790a5
Emmanuel Sanchez <emmannuell.sanchhez@gmail.com>
2025-08-13 18:23:09 -0600

    Correction de los importe de Login y App
    El login se cambio a ruta raiz y se cambia toda la clase de Login con su correcion de los imports

--- FrontEnd/src/pages/Login.jsx
+++ FrontEnd/src/pages/Login.jsx
@@ -1,22 +1,58 @@
 import CustomButton from "../components/CustomButton";
 import "../styles/Login.css";
 import InjaconBlanco from "../img/InjaconBlanco.jpg";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    try {
+      const response = await fetch("http://localhost:3002/api/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+      });
+      const data = await response.json();
+      if (response.ok) {
+        // Login exitoso, mostrar mensaje personalizado
+        localStorage.setItem("rol", data.rol); // 👈 guardar rol
+        alert(`${data.message}, ${data.name}!`);
+        navigate("/app"); // Redirigir a la aplicación principal
+      } else {
+        setError(data.message || "Error al iniciar sesión");
+      }
+    } catch (err) {
+      setError("Error de conexión");
+    }
+  };
+
   return (
     <div className="login-container">
       <img src={InjaconBlanco} alt="Injacom Logo" />
       <h3>Iniciar Sesión</h3>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="email"
           placeholder="Correo"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
           required
         />
         <br />
         <input
           type="password"
           placeholder="Contraseña"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
           required
         />
         <br />
@@ -25,6 +61,7 @@ function Login() {
           type="submit"
           id="save-button"
         />
+        {error && <div style={{ color: "red" }}>{error}</div>}
       </form>
     </div>
   );
